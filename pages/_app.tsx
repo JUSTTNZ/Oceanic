4b8f1d5d44a158f02864d81a3f9cfc8db5a0e9f7
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-03-19 13:19:21 +0100

    new added fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter", // This creates a CSS variable
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={inter.className}>
+       <Component {...pageProps} />
+       </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,8 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+
+
 import Homepage from "./components/home";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
