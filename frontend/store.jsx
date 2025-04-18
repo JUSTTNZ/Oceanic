8781fca99d53ddb3626d80d79281ece26fd1688c
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-04-18 14:29:26 +0100

    configure store

--- frontend/store.jsx
+++ frontend/store.jsx
@@ -1,32 +1,59 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage"; 
-const initialState =  {
-     user:''
-    
-}
-const reducer = ( state = initialState, action) => {
-switch (action.type) {
-        case "SET_USER":
-            return{
-                ...state,
-                user: action.payload
-            }
+import { persistReducer, persistStore, createTransform } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// Set expiration time in milliseconds (1 hour)
+const EXPIRE_AFTER = 60 * 60 * 1000; // 1 hour = 3600000 ms
+
+// Transform to add and check expiry
+const expireTransform = createTransform(
+  // transform state before saving to storage
+  (inboundState, key) => {
+    return {
+      ...inboundState,
+      _persistedAt: Date.now(),
+    };
+  },
+  // transform state being rehydrated
+  (outboundState, key) => {
+    const currentTime = Date.now();
+    const savedTime = outboundState?._persistedAt || 0;
+
+    if (currentTime - savedTime > EXPIRE_AFTER) {
+      // Expired - return initial state
+      return undefined;
+    }
+
+    return outboundState;
+  }
+);
+
+const initialState = {
+  user: '',
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
     default:
-        return state;
-        
-}
+      return state;
+  }
+};
 
-}
 const persistConfig = {
-    key: "root",
-    storage,
+  key: "root",
+  storage,
+  transforms: [expireTransform],
+};
 
-  };
-const persistedReducer = persistReducer(persistConfig,reducer);
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-    reducer: persistedReducer
+  reducer: persistedReducer,
+});
 
-})
 export const persistor = persistStore(store);
