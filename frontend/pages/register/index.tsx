cc6b8a3e8a5e658042f9bcbcec6f4f11f8e300f0
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-04-23 01:10:54 +0100

    fixed validation of register

--- frontend/pages/register/index.tsx
+++ frontend/pages/register/index.tsx
@@ -1,14 +1,11 @@
-import Footer from "../login/footer";
-import Header from "../login/header";
 import RegistePage from "./page";
 
 
 export default function Register(){
     return(
         <div className="min-h-screen bg-gray-100">
-        <Header />
         <RegistePage />
-        <Footer />
+  
         </div>
     )
 }
