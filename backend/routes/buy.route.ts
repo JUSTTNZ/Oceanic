8d876aaa01f55bc8e418fb04a6af13a263dc099d
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-05-14 07:02:07 +0100

    Refactor rate API endpoint to use CoinGecko API and improve error handling; update React and React DOM dependencies to version 18.3.1

--- backend/routes/buy.route.ts
+++ backend/routes/buy.route.ts
@@ -3,6 +3,6 @@ import express from 'express';
 import { handlePaystackWebhook } from '../controllers/paystackwebhook.controller.js';
 const router = express.Router();
 
-router.route('/paystack').post(handlePaystackWebhook);
+router.route('/webhook').post(handlePaystackWebhook);
 
 export default router;
--- frontend/pages/api/rate.ts
+++ frontend/pages/api/rate.ts
@@ -1,15 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-
+import axios from 'axios';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-      const response = await fetch(
-        'https://v6.exchangerate-api.com/v6/3f7dacb1010dc025c1554d33/latest/USD'
-      );
-      const data = await response.json();
-      res.status(200).json(data);
-    } catch (error) {
-        console.error('Error fetching countries:', error);
-      res.status(500).json({ error: "Failed to fetch countries" });
+  try {
+    const response = await axios.get(
+      'https://api.coingecko.com/api/v3/coins/markets',
+      {
+        params: {
+          vs_currency: 'ngn',
+          order: 'market_cap_desc',
+          per_page: 100,
+          page: 1,
+          sparkline: false
+        }
+      }
+    );
+
+    interface Coin {
+      name: string;
+      symbol: string;
+      image: string;
+      current_price: number;
+    }
+
+    const data = response.data.map((coin: Coin) => {
+      const buyRate = coin.current_price;
+      const sellRate = coin.current_price;
+      const margin = 50;
+
+      return {
+        name: coin.name,
+        symbol: coin.symbol.toUpperCase(),
+        image: coin.image,
+        market_price: buyRate,
+        buy_rate: buyRate - margin,
+        sell_rate: sellRate - margin
+      };
+    });
+
+    res.status(200).json({ success: true, data });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Failed to fetch CoinGecko rates:', error.message);
+      res.status(500).json({ success: false, message: 'Failed to fetch crypto rates' });
+    } else {
+      console.error('An unknown error occurred');
+      res.status(500).json({ success: false, message: 'An unknown error occurred' });
     }
   }
+}
