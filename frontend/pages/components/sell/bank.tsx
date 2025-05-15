ee742d5ac89eaa945f7e1e20d579267a11cc8451
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-05-15 15:03:01 +0100

    added

---
+++ frontend/pages/components/sell/bank.tsx
@@ -0,0 +1,155 @@
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import {  useState } from "react";
+
+interface Bank {
+  name: string;
+  code: string;
+}
+
+interface BankDetails {
+  accountNumber: string;
+  accountName: string;
+  bankName: string;
+  bankCode?: string;
+}
+
+interface BanksProps {
+  banksList: Bank[];
+  bankDetails: BankDetails;
+  setBankDetails: React.Dispatch<React.SetStateAction<BankDetails>>;
+  className?: string;
+  status: string;
+}
+
+export default function Banks({
+  banksList,
+  bankDetails,
+  setBankDetails,
+  status,
+}: BanksProps) {
+  const [showBankDropdown, setShowBankDropdown] = useState(false);
+  const [bankSearchTerm, setBankSearchTerm] = useState("");
+  const [selectedBank, setSelectedBank] = useState<Bank | null>(null);
+
+  const filteredBanks = banksList.filter((bank) =>
+    bank.name.toLowerCase().includes(bankSearchTerm.toLowerCase())
+  );
+
+  const handleBankDetailsChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setBankDetails((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className={`space-y-4 `}>
+      <div className="space-y-2">
+        <label className="text-sm font-medium text-gray-100">Bank Name</label>
+        <div className="relative">
+          <button
+            onClick={() => setShowBankDropdown(!showBankDropdown)}
+            className="flex items-center justify-between w-full border border-gray-500 px-4 py-3 rounded-lg text-sm hover:border-gray-600 focus:border-blue-600 focus:outline-none transition-colors text-white"
+            aria-expanded={showBankDropdown}
+            aria-haspopup="listbox"
+          >
+            <span>{selectedBank?.name || "Select your bank"}</span>
+            <ChevronDownIcon
+              className={`h-4 w-4 ml-2 text-white transition-transform ${
+                showBankDropdown ? "rotate-180" : ""
+              }`}
+            />
+          </button>
+
+          {showBankDropdown && (
+            <div
+              className="absolute z-10 mt-1 w-full bg-gray-900 text-white border border-gray-800 rounded-lg shadow-lg"
+              role="listbox"
+            >
+              <div className="p-2 border-b border-blue-600">
+                <input
+                  type="text"
+                  placeholder="Search banks..."
+                  className="w-full px-3 py-2 text-sm border rounded-md focus:border-blue-600 focus:outline-none"
+                  value={bankSearchTerm}
+                  onChange={(e) => setBankSearchTerm(e.target.value)}
+                  autoFocus
+                />
+              </div>
+              <div className="max-h-60 overflow-y-auto scrollbar-hide">
+                {filteredBanks.length > 0 ? (
+                  filteredBanks.map((bank) => (
+                    <button
+                      key={bank.code}
+                      className="w-full text-left px-4 py-2 text-sm hover:bg-gray-800"
+                      onClick={() => {
+                        setBankDetails((prev) => ({
+                          ...prev,
+                          bankName: bank.name,
+                          bankCode: bank.code,
+                        }));
+                        setSelectedBank(bank);
+                        setShowBankDropdown(false);
+                        setBankSearchTerm("");
+                      }}
+                      role="option"
+                      aria-selected={bank.name === selectedBank?.name}
+                    >
+                      {bank.name}
+                    </button>
+                  ))
+                ) : (
+                  <div className="px-4 py-2 text-sm text-gray-100">
+                    No banks found
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <label
+          htmlFor="accountNumber"
+          className="block text-sm font-medium text-gray-100 mb-1"
+        >
+          Account Number
+        </label>
+        <input
+          type="text"
+          id="accountNumber"
+          name="accountNumber"
+          value={bankDetails.accountNumber}
+          onChange={handleBankDetailsChange}
+          placeholder="1234567890"
+          className="w-full border border-gray-500 px-4 py-2 rounded-lg text-white text-md font-medium focus:border-blue-600 focus:outline-none"
+          disabled={status !== "pending"}
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="accountName"
+          className="block text-sm font-medium text-gray-100 mb-1"
+        >
+          Account Name
+        </label>
+        <input
+          type="text"
+          id="accountName"
+          name="accountName"
+          value={bankDetails.accountName}
+          onChange={handleBankDetailsChange}
+          placeholder="John Doe"
+          className="w-full border border-gray-500 px-4 py-2 rounded-lg text-white text-md font-medium focus:border-blue-600 focus:outline-none"
+          disabled={status !== "pending"}
+        />
+      </div>
+    </div>
+  );
+}
+
