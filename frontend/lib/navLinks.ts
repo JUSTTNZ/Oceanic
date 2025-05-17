e2215800f5459348bde733dca3ebb4784743f16b
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-05-17 13:12:18 +0100

    feat: Update navigation links and enhance App component with animations for improved user experience

--- frontend/lib/navLinks.ts
+++ frontend/lib/navLinks.ts
@@ -17,12 +17,12 @@ export const megaMenus = {
   ],
   Resources: [
     { icon: UsersIcon, title: "Community", desc: "Connect with other traders and stay updated on the latest trends.", href: "resources/community" },
-    { icon: RocketLaunchIcon, title: "Tutorials", desc: "Step-by-step guides to help you navigate crypto trading with ease.", href: "resources/tutorials" },
+    { icon: RocketLaunchIcon, title: "Tutorials", desc: "Step-by-step guides to help you navigate crypto trading with ease.", href: "resources/tutorial" },
     { icon: BookOpenIcon, title: "Docs", desc: "Comprehensive technical documentation for developers and traders.", href: "resources/docs" },
   ],
   Company: [
     { icon: BriefcaseIcon, title: "About Us", desc: "Learn more about Oceanic and our mission to revolutionize trading.", href: "company/about" },
-    { icon: BookOpenIcon, title: "Careers", desc: "Join our team and build a future in the world of crypto and finance.", href: "company/careers" },
+    { icon: BookOpenIcon, title: "Careers", desc: "Join our team and build a future in the world of crypto and finance.", href: "company/career" },
     { icon: RocketLaunchIcon, title: "Blog", desc: "Stay informed with the latest news, insights, and market trends.", href: "company/blog" },
   ],
 };
