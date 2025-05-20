22049ac907418c570a289e6015fe49437721af49
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-05-20 11:07:43 +0100

    fix rate api

--- frontend/pages/api/rate.ts
+++ frontend/pages/api/rate.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
       const response = await fetch(
-        'https://v6.exchangerate-api.com/v6/3f7dacb1010dc025c1554d33/latest/USD'
+        'https://v6.exchangerate-api.com/v6/6c726f2eef74105ff040df4b/latest/USD'
       );
       const data = await response.json();
       res.status(200).json(data);
