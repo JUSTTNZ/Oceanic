ba74ec107cbacd95876f9950b53a80d8e6276e18
CHUKWUDI CONFIDENCE OSINACHI <chukwudiconfidenceosinachi@gmail.com>
2025-06-08 08:01:13 +0100

    Update buycrypto.tsx

--- frontend/pages/trade/buycrypto.tsx
+++ frontend/pages/trade/buycrypto.tsx
@@ -11,7 +11,7 @@ import AmountInput from "../components/buy/amout";
 import ConversionDisplay from "../components/buy/conversion";
 import FirstSide from "../components/buy/firstside";
 import WalletAddressBuy from "../components/buy/walletaddress";
-import ErrorDisplay from "../../ui/errorbuy"
+//import ErrorDisplay from "../../ui/errorbuy"
 import LoadingDisplay from '../../ui/loading'
 
 interface Coin {
@@ -312,11 +312,11 @@ useEffect(() => {
      <LoadingDisplay />
     );
   }
-if (error){
+/*if (error){
   return(
  <ErrorDisplay />
   )
-}
+}*/
 
 
 
