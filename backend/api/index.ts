6d9f4c159d8b25c96c086aa5d336a64ec91afd99
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-08-11 20:54:01 +0100

    new3

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,3 +1,24 @@
-//api/index.ts
-import app from "../src/app.js";
-export default app
+// index.ts
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import connectDB from '../src/config/db.js'
+import app from '../src/app.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+dotenv.config({ path: join(__dirname, '.env') })
+
+const PORT = process.env.PORT || 7001
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.error('Error starting server', err)
+    process.exit(1)
+  })
