1e1f7687c1e8c5c0ea9deec37b504a1772eb346b
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2026-02-17 12:52:52 +0100

    fixed issue

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,10 +3,8 @@ import { DB_NAME } from '../constant.js'
 
 const connectDB = async () => {
     try {
-        console.log('🔍 MONGO_URI:', process.env.MONGODB_URI);
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(` \n MongoDB connected ! DB host: ${connectionInstance.connection.host}`);
-        console.log("it works")
+        console.log(`MongoDB connected ! DB host: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection error", error);
         process.exit(1);
